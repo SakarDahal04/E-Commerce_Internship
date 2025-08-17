749f59e6b696385b3f8e05c68cc27459e5f0eb09
Dipesh Regmi <108415886+dipesh7002@users.noreply.github.com>
2025-08-17 22:00:48 +0545

    changes in settings.py

--- ECommerce_Internship/urls.py
+++ ECommerce_Internship/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin 
 from django.urls import path , include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import(
     TokenObtainPairView, TokenRefreshView,
 )
